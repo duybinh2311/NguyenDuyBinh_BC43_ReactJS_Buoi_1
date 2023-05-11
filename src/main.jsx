82674fa8393aa3e149fefc134fc81d93ce4a7e7f
duybinh2311 <duybinh.nguyen2311@gmail.com>
2023-05-11 23:30:27 +0700

    update alias

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import BaiTapThucHanhLayOut from './BaiTapLayoutComponent/BaiTapThucHanhLayOut'
+import Button from '@button'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BaiTapThucHanhLayOut />
+    <Button/>
   </React.StrictMode>
 )
